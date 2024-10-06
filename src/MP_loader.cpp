1437a742e36ebefbb90bf6f2ef13aea28c3ee125
iThorgrim <contact@warlog.dev>
2024-10-06 13:24:43 +0200

    (Refactor): Factorize config file name

--- src/MP_loader.cpp
+++ src/MP_loader.cpp
@@ -8,7 +8,7 @@ void SC_AddRecacheScripts();
 // Add all
 // cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
 // additionally replace all '-' in the module folder name with '_' here
-void AddRecacheScripts()
+void Addmod_recacheScripts()
 {
     SC_AddRecacheScripts();
 }
